9c96127a279e8a6ccff467413f862783c8cb8e63
yap8 <nebozhenko.arseny@gmail.com>
2022-03-24 11:44:29 +0300

    добавили bootstrap

--- info.php
+++ info.php
@@ -15,9 +15,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include_once('partials/head.php'); ?>
   <title>Информация</title>
 </head>
 <body>
