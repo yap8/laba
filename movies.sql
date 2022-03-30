70bb141ad7ed7e5f4176a6ea2aeddad8e71c4f40
yap8 <nebozhenko.arseny@gmail.com>
2022-03-30 16:55:01 +0300

    обновлён дапм бд

--- movies.sql
+++ movies.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 10, 2022 at 12:15 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 30, 2022 at 04:53 PM
+-- Server version: 5.5.62
+-- PHP Version: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,16 +32,78 @@ CREATE TABLE `movies` (
   `title` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   `year` varchar(255) NOT NULL,
-  `info` text NOT NULL
+  `info` text NOT NULL,
+  `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `movies`
 --
 
-INSERT INTO `movies` (`id`, `title`, `country`, `year`, `info`) VALUES
-(1, 'movie #1', 'russia', '2022', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-(2, 'movie #2', 'russia', '2012', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+INSERT INTO `movies` (`id`, `title`, `country`, `year`, `info`, `status`) VALUES
+(20, 'asdfasdfasdf', 'Afghanistan', '2022', 'asdfasdfasdfdasf\r\n', 'в прокате'),
+(21, '1111111', 'Afghanistan', '2022', '123123213123', 'прокат окончен'),
+(22, 'testtest', 'Anguilla', '2022', '3333333333333', 'прокат окончен'),
+(23, '1231233333', 'Afghanistan', '2022', 'fsadfsdafsadffsdfasf', 'анонсирован'),
+(24, '1111111', 'Afghanistan', '1990', '123123123123123', 'прокат окончен'),
+(25, '333333', 'Russian Federation', '2022', '', 'прокат окончен');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statuses`
+--
+
+CREATE TABLE `statuses` (
+  `анонсирован` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `в прокате` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `прокат закончен` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `types`
+--
+
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`id`, `name`) VALUES
+(1, 'user'),
+(2, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type_id` char(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `type_id`) VALUES
+(1, 'Bodea', '123123', '2'),
+(2, 'senya', '123123', '1'),
+(3, 'asdf', '123', '1'),
+(4, '123123', '123123213', '1'),
+(5, '123123', '123123213', '1'),
+(6, 'Ars', '54321', '1'),
+(7, '1111', '1111', '1');
 
 --
 -- Indexes for dumped tables
@@ -54,6 +116,18 @@ ALTER TABLE `movies`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,7 +135,19 @@ ALTER TABLE `movies`
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `types`
+--
+ALTER TABLE `types`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
